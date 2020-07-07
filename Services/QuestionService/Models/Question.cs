ffbabf43998b0df73ed443837d5aeb8b174695dc
Pramod Mekapothula <m.pramodreddy123@gmail.com>
2020-07-06 19:03:38 -0700

    Adding models

---
+++ Services/QuestionService/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuestionService.Models
+{
+    public class Question
+    {
+        public static readonly string DocumentName = "QuestionBank";
+
+        public Guid QuestionId { get; set; }
+        public string Description { get; set; }
+        public List<string> Options { get; set; }
+        public string Answer { get; set; }
+        public string Tag { get; set; }
+    }
+}

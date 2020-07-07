5d9ce2caba8d975d1a5c0b05d5f070d0ad850d45
Pramod Mekapothula <m.pramodreddy123@gmail.com>
2020-07-06 19:11:02 -0700

    Adding repository layer

---
+++ Services/QuestionService/Repositories/IQuestionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using QuestionService.Models;
+
+namespace QuestionService.Repositories
+{
+    public interface IQuestionRepository
+    {
+        List<Question> GetQuestionQuestions();
+        Question GetQuestionQuestion(Guid questionId);
+        void InsertQuestionQuestion(Question question);
+        void UpdateQuestionQuestion(Question question);
+        void DeleteQuestionQuestion(Guid questionId);
+    }
+}
---
+++ Services/QuestionService/Repositories/QuestionRepository.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using QuestionService.Models;
+using System;
+using System.Collections.Generic;
+
+namespace QuestionService.Repositories
+{
+    public class QuestionRepository : IQuestionRepository
+    {
+        private readonly IMongoCollection<Question> _repository;
+
+        public QuestionRepository(IMongoDatabase db)
+        {
+            _repository = db.GetCollection<Question>(Question.DocumentName);
+        }
+
+        public void DeleteQuestionQuestion(Guid questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Question GetQuestionQuestion(Guid questionId)
+        {
+            return _repository.Find(x => x.QuestionId == questionId).FirstOrDefault();
+        }
+
+        public List<Question> GetQuestionQuestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertQuestionQuestion(Question question)
+        {
+            _repository.InsertOne(question);
+        }
+
+        public void UpdateQuestionQuestion(Question question)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
